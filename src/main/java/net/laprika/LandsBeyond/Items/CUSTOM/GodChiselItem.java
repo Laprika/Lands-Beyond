610666463f3e132d0aa1970cb559f3663e20f6f9
Laprika <trxthsxxkxr@gmail.com>
2025-12-07 01:26:24 +0100

    Changes.

--- src/main/java/net/laprika/LandsBeyond/Items/CUSTOM/GodChiselItem.java
+++ src/main/java/net/laprika/LandsBeyond/Items/CUSTOM/GodChiselItem.java
@@ -1,8 +1,6 @@
 package net.laprika.LandsBeyond.Items.CUSTOM;
 
-import net.laprika.LandsBeyond.block.ModBlocks;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionResult;
@@ -20,16 +18,8 @@ import net.minecraft.world.phys.Vec3;
 import java.util.Map;
 
 public class GodChiselItem extends Item {
-    private static final Map<Block, Block> CHISEL_MAP =
-            Map.of(
-                    Blocks.STONE, Blocks.DIAMOND_ORE,
-                    Blocks.COPPER_BLOCK, Blocks.IRON_BLOCK,
-                    Blocks.IRON_BLOCK, Blocks.GOLD_BLOCK,
-                    Blocks.GOLD_BLOCK, Blocks.DIAMOND_BLOCK,
-                    Blocks.DIAMOND_BLOCK, Blocks.NETHERITE_BLOCK,
-                    Blocks.AMETHYST_BLOCK, Blocks.BUDDING_AMETHYST,
-                    Blocks.OAK_LOG, Blocks.COAL_BLOCK
-            );
+    private static final Map<Block, Block> CHISEL_MAP = Map.of(Blocks.STONE, Blocks.DIAMOND_ORE, Blocks.COPPER_BLOCK, Blocks.IRON_BLOCK, Blocks.IRON_BLOCK, Blocks.GOLD_BLOCK, Blocks.GOLD_BLOCK, Blocks.DIAMOND_BLOCK, Blocks.DIAMOND_BLOCK, Blocks.NETHERITE_BLOCK, Blocks.AMETHYST_BLOCK, Blocks.BUDDING_AMETHYST, Blocks.OAK_LOG, Blocks.COAL_BLOCK);
+
     public GodChiselItem(Properties properties) {
         super(properties);
     }
@@ -39,22 +29,21 @@ public class GodChiselItem extends Item {
         Level level = context.getLevel();
         Block clickedBlock = level.getBlockState(context.getClickedPos()).getBlock();
 
-        if(CHISEL_MAP.containsKey(clickedBlock)) {
-            if(!level.isClientSide) {
+        if (CHISEL_MAP.containsKey(clickedBlock)) {
+            if (!level.isClientSide) {
 
-                level.setBlockAndUpdate(context.getClickedPos(),CHISEL_MAP.get(clickedBlock).defaultBlockState());
+                level.setBlockAndUpdate(context.getClickedPos(), CHISEL_MAP.get(clickedBlock).defaultBlockState());
 
-                context.getItemInHand().hurtAndBreak(1,((ServerLevel) level), context.getPlayer(),
-                        item -> context.getPlayer().onEquippedItemBroken(item, EquipmentSlot.MAINHAND));
+                context.getItemInHand().hurtAndBreak(1, ((ServerLevel) level), context.getPlayer(), item -> context.getPlayer().onEquippedItemBroken(item, EquipmentSlot.MAINHAND));
 
-                if(Math.random() < 0.001F) {
+                if (Math.random() < 0.001F) {
                     Vec3 playerPos = context.getPlayer().position();
                     context.getLevel().explode(context.getPlayer(), playerPos.x, playerPos.y, playerPos.z, 20F, true, Level.ExplosionInteraction.MOB);
                 }
 
-                if(Math.random() < 0.001F) {
+                if (Math.random() < 0.001F) {
                     Player player = context.getPlayer();
-                    for(int i = 0; i < 100; i++) {
+                    for (int i = 0; i < 100; i++) {
 
                         Creeper creeper = new Creeper(EntityType.CREEPER, level);
                         creeper.setPos(player.position());
@@ -64,14 +53,11 @@ public class GodChiselItem extends Item {
                 }
 
 
-                }
-
-
-                level.playSound(null, context.getClickedPos(), SoundEvents.AMETHYST_BLOCK_CHIME, SoundSource.BLOCKS);
             }
+
+
+            level.playSound(null, context.getClickedPos(), SoundEvents.AMETHYST_BLOCK_CHIME, SoundSource.BLOCKS);
         }
-
-
 
 
         return InteractionResult.SUCCESS;
